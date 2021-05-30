6e27d97f03529f2819e49a98d3928c291ae4cf5e
dhruvidesai20 <71321899+dhruvidesai20@users.noreply.github.com>
2021-05-30 16:49:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-  function imgProperties(){
+ function imgProperties(){
     if ($(window).width() > 426){
       var width= $(".boyFace").width()- 75;
       $(".mask-img").css("width", width);
@@ -18,8 +18,12 @@
 
   var controller= new ScrollMagic.Controller();
 
-  var timeline1= new TimelineMax();
-  timeline1
+  var scene1= new ScrollMagic.Scene({
+
+  })
+
+  var timeline2= new TimelineMax();
+  timeline2
   .from(".mask", 4, {
     y: 450,
     ease: Power1.easeInOut
@@ -29,46 +33,65 @@
     ease: Power1.easeInOut
   }, "-=3")
 
-  var scene1= new ScrollMagic.Scene({
+  var scene2= new ScrollMagic.Scene({
     triggerElement: "#trigger",
     duration: "100%",
     triggerHook: 0,
   })
-  .setTween(timeline1)
+  .setTween(timeline2)
   .setPin("#trigger")
+  .addTo(controller)
+
+  var timeline3= new TimelineMax();
+  timeline3
+    .to(".bubble1", 4, {
+      opacity:1,
+      transform: "translateY(0)"
+    })
+    .to(".bubble2", 4, {
+      opacity:1,
+      transform: "translateY(0)"
+    }, "-=3")
+    .to(".bubble3", 4, {
+      opacity:1,
+      transform: "translateY(0)"
+    }, "-=2")
+    .to(".bubble4", 4, {
+      opacity:1,
+      transform: "translateY(0)"
+    }, "-=1")
+  var scene3= new ScrollMagic.Scene({
+    triggerElement: ".section3",
+    duration: "100%",
+    triggerHook: 0
+  })
+  .setTween(timeline3)
+  .setPin(".section3")
+  .addTo(controller)
+
+  var scene5= new ScrollMagic.Scene({
+    triggerElement: ".section5",
+    duration: "30%",
+    triggerHook: 0
+  })
+
+  .setPin(".section5")
+  .addTo(controller)
+
+  var scene6= new ScrollMagic.Scene({
+    triggerElement: ".section6",
+    duration: "30%",
+    triggerHook: 0
+  })
+
+  .setPin(".section6")
   .addTo(controller)
 
   function scroll(){
     if($(window).width()> 426){
-      var timeline2= new TimelineMax();
-      timeline2
-        .to(".bubble1", 4, {
-          opacity:1,
-          transform: "translateY(0)"
-        })
-        .to(".bubble2", 4, {
-          opacity:1,
-          transform: "translateY(0)"
-        }, "-=3")
-        .to(".bubble3", 4, {
-          opacity:1,
-          transform: "translateY(0)"
-        }, "-=2")
-        .to(".bubble4", 4, {
-          opacity:1,
-          transform: "translateY(0)"
-        }, "-=1")
-      var scene2= new ScrollMagic.Scene({
-        triggerElement: ".section3",
-        duration: "100%",
-        triggerHook: 0
-      })
-      .setTween(timeline2)
-      .setPin(".section3")
-      .addTo(controller)
 
-      var timeline3= new TimelineMax();
-      timeline3
+      var timeline4= new TimelineMax();
+      timeline4
       .to(".sec405", 5, {
         transform: "translateX(0)"
       })
@@ -89,31 +112,13 @@
         transform: "translateX(0)"
       }, "-=1")
 
-        var scene3= new ScrollMagic.Scene({
+        var scene4= new ScrollMagic.Scene({
           triggerElement: ".section4",
           duration: "100%",
           triggerHook: 0
         })
-        .setTween(timeline3)
+        .setTween(timeline4)
         .setPin(".section4")
-        .addTo(controller)
-
-        var scene4= new ScrollMagic.Scene({
-          triggerElement: ".section5",
-          duration: "30%",
-          triggerHook: 0
-        })
-
-        .setPin(".section5")
-        .addTo(controller)
-
-        var scene5= new ScrollMagic.Scene({
-          triggerElement: ".section6",
-          duration: "30%",
-          triggerHook: 0
-        })
-
-        .setPin(".section6")
         .addTo(controller)
     }
   }
